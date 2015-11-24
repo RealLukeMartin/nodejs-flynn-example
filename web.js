a78c1ff2a4ad6c3ca25c3227550d6b0b8f234b08
RealLukeMartin <lukemartinwebdev@gmail.com>
2015-11-24 14:17:15 -0600

    add current hour

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var http = require('http');
 var port = process.env.PORT || 5000;
+var date = new Date();
+var current_hour = date.getHours();
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('THIS IS A TEST BRANCH YO YO YO YO YO YO YO YO YO YO YO YO YO YO YO YO Flynn on port '+port+' from container '+process.env.HOSTNAME+'\n');
+  res.end('THIS IS A TEST BRANCH. The time is '+current_hour+' Flynn on port '+port+' from container '+process.env.HOSTNAME+'\n');
 }).listen(port, function() {
   console.log("Listening on " + port);
 });
